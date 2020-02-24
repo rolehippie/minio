2b6af1cde820bf3965ea4b890b4c362b3c0bad66
Thomas Boerger <thomas@webhippie.de>
2020-02-24 10:23:56 +0100

    Add more common test naming

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,21 +7,21 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts('all')
 
 
-def test_minio_running_and_enabled(host):
+def test_running_and_enabled(host):
     svc = host.service("minio")
     assert svc.is_running
     assert svc.is_enabled
 
 
-def test_minio_socket(host):
+def test_server_socket(host):
     assert host.socket("tcp://127.0.0.1:9000").is_listening
 
 
-def test_minio_client_file(host):
+def test_client_file(host):
     file = host.file("/usr/local/bin/mc")
     assert file.exists
 
 
-def test_minio_client_exec(host):
+def test_client_exec(host):
     cmd = host.run("mc --help")
     assert cmd.succeeded
